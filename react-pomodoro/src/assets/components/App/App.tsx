cade3bc0e0dc58c9d404291cbf1efc6468d19b95
matheuscamargo <matheuscamargodev@gmail.com>
2023-03-15 20:24:00 -0300

    buttons of Clock

--- react-pomodoro/src/assets/components/App/App.tsx
+++ react-pomodoro/src/assets/components/App/App.tsx
@@ -5,7 +5,7 @@ export default function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="container bg-white rounded col-6 mt-5 p-5">
+    <div className="container bg-white rounded col-9 col-md-6 mt-5 p-5">
       <Pomodoro defaultTimer={1500}/>
     </div>
   )
--- react-pomodoro/src/assets/components/Pomodoro/index.tsx
+++ react-pomodoro/src/assets/components/Pomodoro/index.tsx
@@ -1,19 +1,52 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useInterval } from '../../hooks/use-interval';
 import Clock from '../Clock';
-import {FaClock} from 'react-icons/fa';
+import {FaClock, FaPlay, FaStop} from 'react-icons/fa';
 interface Props{
   defaultTimer: number;
 }
+
 export default function Pomodoro(props: Props){
   const [mainTime, setMainTime] = useState(props.defaultTimer)
+  const [initCount, setInitCount] = useState(false);
+  const [working, setWorking] = useState(false);
+
+  useEffect(() => {
+    const buttons = document.querySelectorAll('.btn');
+    if(working){
+      document.body.classList.remove('bg-dark')
+      document.body.classList.add('bg-danger')
+      buttons.forEach(button => {
+        button.classList.remove('bg-danger');
+        button.classList.add('bg-secondary')
+      })
+    }else{
+      document.body.classList.add('bg-dark')
+      document.body.classList.remove('bg-danger')
+      buttons.forEach(button => {
+        button.classList.remove('bg-secondary')
+        button.classList.add('bg-danger');
+      })
+
+    }
+  }, [working]);
+
+  const configureWork = (r: boolean):void =>{
+    setInitCount(r);
+    setWorking(r);
+  }
   useInterval(() => {
     setMainTime(mainTime - 1);
-  }, 1000)
+  }, initCount ? 1000 : null)
   return(
     <div className="text-dark text-center">
       <h2>You are working <FaClock /></h2>
       <Clock mainTime={mainTime}/>
+      <div className="d-flex display-buttons justify-content-center ms-5 me-5 mt-5">
+        <button className="btn btn-danger" onClick={() => configureWork(!working)}>{ working ? 'Resting' : 'Working'}</button>
+        <button className="btn btn-danger ms-1 me-1 align-items-center" onClick={() => configureWork(!working)}>{ working ? <FaStop/> : <FaPlay/>}</button>
+        <button className="btn btn-danger" onClick={() => setMainTime(1500)}>Restart</button>
+      </div>
     </div>
   )
 }
