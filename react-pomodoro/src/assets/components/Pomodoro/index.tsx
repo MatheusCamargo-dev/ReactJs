313dd40636dc64338384db52301b277a108c8445
matheuscamargo <matheuscamargodev@gmail.com>
2023-03-15 18:57:02 -0300

    function seconds to time (rea

--- react-pomodoro/src/assets/components/Pomodoro/index.tsx
+++ react-pomodoro/src/assets/components/Pomodoro/index.tsx
@@ -1,17 +1,17 @@
 import { useState } from 'react'
 import { useInterval } from '../../hooks/use-interval';
+import { secondsToTime } from '../../utils/secondsToTime';
 interface Props{
   defaultTimer: number;
 }
 export default function Pomodoro(props: Props){
   const [mainTime, setMainTime] = useState(props.defaultTimer)
-
   useInterval(() => {
     setMainTime(mainTime - 1);
   }, 1000)
   return(
     <div className="text-white">
-      <h1>Count: {mainTime}</h1>
+      <h1>Count: {secondsToTime(mainTime)}</h1>
     </div>
   )
 }
