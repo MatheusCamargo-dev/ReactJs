437be7c24ff8f989195720f421b3064bf404b772
matheuscamargo <matheuscamargodev@gmail.com>
2023-03-18 22:14:21 -0300

    Search Posts

--- react-basic/react-lessons/src/modules/Posts/index.tsx
+++ react-basic/react-lessons/src/modules/Posts/index.tsx
@@ -3,11 +3,16 @@ import fetchPosts from '../../utils/load-posts';
 import fetchAnimes from '../../utils/load-anime-posts';
 import Pagination from '../Pagination';
 import Post from '../Post';
+import { FaSearch } from 'react-icons/fa';
 interface Post{
   id: number;
   title: string;
   body: string;
   img?: string;
+}
+
+interface InputChangeEvent extends Event {
+  target: HTMLInputElement;
 }
 
 export default function Posts() {
@@ -16,6 +21,7 @@ export default function Posts() {
   const [allPosts, setAllPosts] = useState<Post[]>([]);
   const [postPerPage, setPostPerPage] = useState(10);
   const [offset, setOffset] = useState(0);
+  const [search, setSearch] = useState<string>();
 
   const loadPosts = async () => {
     const postAndPhotos = await fetchPosts();
@@ -31,18 +37,42 @@ export default function Posts() {
     setPosts(allPosts.slice(offset, postPerPage + offset));
   },[offset])
   
+  useEffect(() => {
+    if(search){
+      const postsFilter = allPosts.filter(post => post.title.startsWith(search))
+      setOffset(0);
+      setPosts(postsFilter.slice(offset, postPerPage))
+    }else{
+      setOffset(0);
+      setPosts(allPosts.slice(offset, postPerPage));
+    }
+  },[search])
+  const handleChange = (e: any) => {
+    const { value } = e.target;
+    setSearch(value);
+  }
   return (
     <div className="posts-content w-100 vh-100 d-flex flex-column">
-
+        <h2 className='text-white text-center mt-3'>Search for posts:</h2>
+        <div className="input-group d-flex justify-content-center vh-0 mt-1">
+          <div className="form-outline" style={{ height: "38px" }}>
+            <input type="search" value={search} onChange={handleChange} placeholder="Search..."className="form-control" style={{borderTopRightRadius: 0, borderBottomRightRadius: 0}} key="form2" />
+          </div>
+          <button type="button" className="btn btn-primary btn-sm">
+            <FaSearch />
+          </button>
+        </div>
         <div className="col bg-dark w-100 text-white p-5" style={{
             display: 'grid',
             gridTemplateColumns: 'repeat(auto-fill, minmax(280px, 1fr))',
             gap: '30px'
           }}
         >
-              {posts.map((post: Post) => (
-                  <Post key={post.id} post={post}/>
-              ))}
+              { posts.length > 0 ?
+                posts.map((post: Post) => (
+                    <Post key={post.id} post={post}/>
+                )) : <h1 className='text-nowrap'>search: "{search}" NOT FOUND.</h1>
+              }
         </div>
         
         <Pagination totalItems={allPosts?.length} limit={postPerPage} offset={offset} setOffset={setOffset} />
