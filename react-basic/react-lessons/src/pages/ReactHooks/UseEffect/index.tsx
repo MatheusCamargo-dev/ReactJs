2fe362b7e8620dd16a49f3c2f1a18b95cf4cb883
Matheus Romeiro Camargo <matheuscamargodev@gmail.com>
2023-03-21 17:48:39 -0300

    React hooks useEffect and useCallback

---
+++ react-basic/react-lessons/src/pages/ReactHooks/UseEffect/index.tsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect, useCallback} from 'react'
+
+interface PropsButton{
+  decrementButton: any
+}
+const Button = React.memo((props: PropsButton) => {
+  console.log('render children')
+  const { decrementButton } = props;
+  return <button className='btn btn-info' onClick={() => decrementButton(1) }>-</button>
+})
+export default function UseEffect() {
+    const [counter, setCounter] = useState(0);
+    
+    useEffect(() => {
+      console.log('componentDidUpdate');
+    });
+    
+    useEffect(() => {
+      console.log('componentDidMount'); //NOTE: If you use React.StrictMode this log will be displayed twice
+    }, []);
+    
+    useEffect(() => {
+      console.log(`Counter change for ${counter}`);
+    }, [counter]);
+
+    const decrementButton = useCallback((num: number) =>{
+      setCounter(c => c - num)
+    }, []);
+
+    return (
+      <>
+       <div className="d-flex mt-5 flex-column align-items-center justify-content-center">
+          <h1>Counter: {counter}</h1>
+          <div className="d-flex gap-3">
+            <button className='btn btn-danger' onClick={() => setCounter(counter + 1)}>+</button>
+            <Button decrementButton={decrementButton}/>
+
+          </div>
+      </div>
+      </>
+    )
+}
--- react-basic/react-lessons/src/pages/ReactHooks/index.tsx
+++ react-basic/react-lessons/src/pages/ReactHooks/index.tsx
@@ -1,4 +1,5 @@
-import React, { useRef, useState} from 'react'
+import React, { useRef, useState, useEffect} from 'react'
+import UseEffect from './UseEffect'
 import UseState from './UseState'
 
 export default function ReactHooks() {
@@ -8,6 +9,8 @@ export default function ReactHooks() {
         <div className="col-12">
             <h1 className='text-center text-info mt-5'>React Hook: useState</h1>
             <UseState />
+            <h1 className='text-center text-info mt-5'>React Hook: useEffect and useCallback</h1>
+            <UseEffect/>
         </div>
     </div>
   )
