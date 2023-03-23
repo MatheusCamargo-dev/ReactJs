60e25aa71d33d8eac963d39286c58d28b41fa317
Matheus Romeiro Camargo <matheuscamargodev@gmail.com>
2023-03-23 16:18:02 -0300

    ReactHooksReact Hooks useContext

--- react-basic/react-lessons/src/pages/ReactHooks/UseContext/index.tsx
+++ react-basic/react-lessons/src/pages/ReactHooks/UseContext/index.tsx
@@ -1,48 +1,19 @@
 import { ReactNode , useContext, useState, createContext} from 'react'
-interface GlobalState {
-  title: string;
-  counter: number;
-}
-const globalState:GlobalState = {
-  title: 'This is my title: ',
-  counter: 0
-}
-
-
-
-
-const Container = ({children}: {children: ReactNode}) => {
-  return <div className="d-flex justify-content-center align-items-center">{children}</div>
-}
-
+import { Container } from './Container';
+import { H1 } from './H1';
+import { Div } from './Div';
+import { globalState, GlobalState} from '../../../store/GlobalContext';
+import { AppContext } from '../../../store/AppContext';
 
 export default function UseContext() {
-  const [contextState, setContextState] = useState<GlobalState>(globalState);
-  const GlobalContext = createContext({contextState, setContextState});
 
-
-
-  const Div = ({children}: {children: ReactNode}) => {
-    const context = useContext(GlobalContext);
-    const {contextState} = context;
-
-    return <div className="App bg-white p-3 rounded"><h1>Counter:{contextState.counter}</h1>{children}</div>
-  }
-
-  const H1 = ({text}: { text: string}) => {
-    const context = useContext(GlobalContext);
-    const {contextState, setContextState} = context;
-
-
-    return <h1 onClick={() => setContextState({...contextState, counter: contextState.counter + 1})} >{contextState.title}{text}</h1>
-  }
   return (
-    <GlobalContext.Provider value={{ contextState, setContextState }}>
+    <AppContext>
       <Container>
         <Div>
           <H1 text='Hello world'></H1>
         </Div>
       </Container>
-    </GlobalContext.Provider>
+    </AppContext>
   )
 }
---
+++ react-basic/react-lessons/src/store/GlobalContext/index.tsx
@@ -0,0 +1,8 @@
+export interface GlobalState {
+    title: string;
+    counter: number;
+}
+export const globalState:GlobalState = {
+    title: 'This is my title: ',
+    counter: 0
+}
