219979241cccfceaa4806dce9270450e7e5991b8
Matheus Romeiro Camargo <matheuscamargodev@gmail.com>
2023-03-29 14:27:26 -0300

    register component v1

--- nextjs/src/components/Login/index.tsx
+++ nextjs/src/components/Login/index.tsx
@@ -85,11 +85,11 @@ export default function Login (props: Login) {
                         </label>
                     </div>
 
-                    <div className="text-sm">
+                    {/* <div className="text-sm">
                         <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
                         Forgot your password?
                         </a>
-                    </div>
+                    </div> */}
                     </div>
 
                     <div>
--- nextjs/src/components/Register/index.tsx
+++ nextjs/src/components/Register/index.tsx
@@ -8,14 +8,44 @@ export default function Register (props: Register) {
     const { changeForm } = props;
 
     return(
-            <div className="w-full max-w-md text-green-800 space-y-8">
-                <h1>REGISTER TEST</h1>
-                <p className="mt-2 text-center text-sm text-white">
-                        Or{' '}
-                        <a onClick={changeForm} className="font-medium cursor-pointer text-green-600 hover:text-green-500">
-                            sign to login
-                        </a>
-                    </p>
+            <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
+                <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
+                    <h1 className="mb-8 text-3xl text-center">Sign up</h1>
+                    <input 
+                        type="text"
+                        className="block border border-grey-light w-full p-3 rounded mb-4"
+                        name="fullname"
+                        placeholder="Full Name" />
+
+                    <input 
+                        type="text"
+                        className="block border border-grey-light w-full p-3 rounded mb-4"
+                        name="email"
+                        placeholder="Email" />
+
+                    <input 
+                        type="password"
+                        className="block border border-grey-light w-full p-3 rounded mb-4"
+                        name="password"
+                        placeholder="Password" />
+                    <input 
+                        type="password"
+                        className="block border border-grey-light w-full p-3 rounded mb-4"
+                        name="confirm_password"
+                        placeholder="Confirm Password" />
+
+                    <button
+                        type="submit"
+                        className="group relative flex w-full justify-center rounded-md bg-green-600 py-2 px-3 text-sm font-semibold text-slate-700 hover:bg-green-500 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >Create Account</button>
+                </div>
+
+                <div className="text-white mt-6">
+                    Already have an account?  
+                    <a onClick={changeForm} className="font-medium cursor-pointer text-green-600 hover:text-green-500">
+                        Log in
+                    </a>
+                </div>
             </div>
     )
 }
