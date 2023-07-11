12edfba8e7e434efc9730eb79eca28ff24409de1
matheuscamargo <matheuscamargodev@gmail.com>
2023-07-10 21:07:57 -0300

    Radio Button Component and init json server

--- rocketseat/03-dt-money/src/components/NewTransactionModal/index.tsx
+++ rocketseat/03-dt-money/src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,12 @@
 import * as Dialog from '@radix-ui/react-dialog'
-import { CloseButton, Content, Overlay } from './styles'
-import { X } from 'phosphor-react'
+import {
+  CloseButton,
+  Content,
+  Overlay,
+  TransactionType,
+  TransactionTypeButton,
+} from './styles'
+import { ArrowCircleUp, X } from 'phosphor-react'
 
 export default function NewTransactionModal() {
   return (
@@ -13,6 +19,17 @@ export default function NewTransactionModal() {
           <input type="text" placeholder="Description" required />
           <input type="text" placeholder="Price" required />
           <input type="text" placeholder="Category" required />
+
+          <TransactionType>
+            <TransactionTypeButton variant="income" value="income">
+              <ArrowCircleUp size={24} />
+              Income
+            </TransactionTypeButton>
+            <TransactionTypeButton variant="expense" value="expense">
+              <ArrowCircleUp size={24} />
+              Expense
+            </TransactionTypeButton>
+          </TransactionType>
           <button type="submit">Register</button>
         </form>
 
