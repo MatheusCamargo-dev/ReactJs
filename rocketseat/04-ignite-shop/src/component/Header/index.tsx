3226c2126212b45d6f97f25ab9a7e02d372b20bb
matheuscamargo <matheuscamargodev@gmail.com>
2023-07-25 22:46:36 -0300

    feat: next image

--- rocketseat/04-ignite-shop/src/component/Header/index.tsx
+++ rocketseat/04-ignite-shop/src/component/Header/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Container, Header as HeaderContainer} from '../../styles/pages/app'
 import logoImg from '../../assets/logo.svg'
+import Image from 'next/image'
 
 export default function Header() {
   return (
     <Container>
         <HeaderContainer>
-            <img src={logoImg.src} alt="" />
+            <Image src={logoImg} alt="" />
          </HeaderContainer>
     </Container>
    
