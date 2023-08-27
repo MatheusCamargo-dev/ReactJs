849eb869e264c67bce0eed8e12f4d695c9cc8233
MatheusCamargo <matheuscamargodev@gmail.com>
2023-08-26 21:49:18 -0300

    feat: User validation

--- rocketseat/ignite-call/src/pages/api/users/index.api.ts
+++ rocketseat/ignite-call/src/pages/api/users/index.api.ts
@@ -2,6 +2,7 @@
 
 import { NextApiRequest, NextApiResponse } from 'next'
 import { prisma } from '../../../lib/prisma'
+import { setCookie } from 'nookies'
 
 export default async function handler(
   req: NextApiRequest,
@@ -13,11 +14,28 @@ export default async function handler(
 
   const { name, username } = req.body
 
+  const userExists = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (userExists) {
+    return res.status(400).json({
+      message: 'User already exists.',
+    })
+  }
+
   const user = await prisma.user.create({
     data: {
       name,
       username,
     },
+  })
+
+  setCookie({ res }, '@ignitecall:userId', user.id, {
+    maxAge: 60 * 60 * 24 * 7, // 7 Days
+    path: '/',
   })
   return res.status(201).json(user)
 }
