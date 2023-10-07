b7b06ffa8bbc975b11e6da848a31c05153cecffc
MatheusCamargo <matheuscamargodev@gmail.com>
2023-10-06 23:16:13 -0300

    feat: finish query for blocked dates

--- rocketseat/ignite-call/src/components/Calendar/index.tsx
+++ rocketseat/ignite-call/src/components/Calendar/index.tsx
@@ -26,6 +26,7 @@ interface CalendarProps {
 
 interface BlockedDates {
   blockedWeekDays: number[]
+  blockedDates: number[]
 }
 
 type CalendarWeeks = CalendarWeek[]
@@ -59,7 +60,7 @@ export function Calendar({ onDateSelected, selectedDate }: CalendarProps) {
       const response = await api.get(`/users/${username}/blocked-dates`, {
         params: {
           year: currentDate.get('year'),
-          month: currentDate.get('month'),
+          month: currentDate.get('month') + 1,
         },
       })
 
@@ -103,7 +104,9 @@ export function Calendar({ onDateSelected, selectedDate }: CalendarProps) {
       ...daysInMonthArray.map((date) => {
         const isDisabled =
           date.endOf('day').isBefore(new Date()) ||
-          blockedDates?.blockedWeekDays.includes(date.get('day'))
+          blockedDates?.blockedWeekDays.includes(date.get('day')) ||
+          blockedDates?.blockedDates.includes(date.get('date'))
+
         return { date, disabled: isDisabled }
       }),
       ...nextMonthFillArray.map((date) => {
