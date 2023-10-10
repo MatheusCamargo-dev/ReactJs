29b682fd98f3534f8c09b9dcaabeaa993a35a968
MatheusCamargo <matheuscamargodev@gmail.com>
2023-10-09 23:58:40 -0300

    feat: schedule api

---
+++ rocketseat/ignite-call/src/pages/api/users/[username]/schedule.api.ts
@@ -0,0 +1,37 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+import dayjs from 'dayjs'
+import { prisma } from '../../../../lib/prisma'
+import { z } from 'zod'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'POST') {
+    return res.status(405).end()
+  }
+
+  const username = String(req.query.username)
+
+  const createSchedulingBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    observations: z.string(),
+    date: z.string().datetime(),
+  })
+
+  const { name, email, observations, date } = req.body
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (!user) {
+    return res.status(400).json({ message: 'User does not exist.' })
+  }
+
+  return res.json({})
+}
