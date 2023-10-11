40c851b559436752840c850ada034dbc1154bea7
MatheusCamargo <matheuscamargodev@gmail.com>
2023-10-10 22:19:14 -0300

    feat: schedule route

--- rocketseat/ignite-call/src/pages/api/users/[username]/schedule.api.ts
+++ rocketseat/ignite-call/src/pages/api/users/[username]/schedule.api.ts
@@ -14,15 +14,6 @@ export default async function handler(
 
   const username = String(req.query.username)
 
-  const createSchedulingBodySchema = z.object({
-    name: z.string(),
-    email: z.string().email(),
-    observations: z.string(),
-    date: z.string().datetime(),
-  })
-
-  const { name, email, observations, date } = req.body
-
   const user = await prisma.user.findUnique({
     where: {
       username,
@@ -33,5 +24,47 @@ export default async function handler(
     return res.status(400).json({ message: 'User does not exist.' })
   }
 
-  return res.json({})
+  const createSchedulingBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    observations: z.string(),
+    date: z.coerce.date(),
+  })
+
+  const { name, email, observations, date } = createSchedulingBodySchema.parse(
+    req.body,
+  )
+
+  const schedulingDate = dayjs(date).startOf('hour')
+
+  if (schedulingDate.isBefore(new Date())) {
+    return res.status(400).json({
+      message: 'Date is in the past.',
+    })
+  }
+
+  const conflictingScheduling = await prisma.scheduling.findFirst({
+    where: {
+      user_id: user.id,
+      date: schedulingDate.toDate(),
+    },
+  })
+
+  if (conflictingScheduling) {
+    return res.status(400).json({
+      message: 'There is another scheduling at the same time.',
+    })
+  }
+
+  await prisma.scheduling.create({
+    data: {
+      date: schedulingDate.toDate(),
+      name,
+      email,
+      observations,
+      user_id: user.id,
+    },
+  })
+
+  return res.status(201).end()
 }
